08a9939d9c9e41ca8488fedd2f1fe40a80d192ac
Alejandro Reyes <diego_1394_rg@hotmail.com>
2017-08-18 20:22:47 -0500

    Actualizacion 1
    Comienzo sobre animaciones y cronometro del juego y algunas ideas para
    guardar el puntaje,

--- js/app.js
+++ js/app.js
@@ -1,7 +1,71 @@
-$("#iniciar_Juego").click(function()
+//Texto Cambiando de Color //
+var textocolor = document.getElementById('titulo_Juego');
+
+function color_uno() 
+{
+    textocolor.style.color='#DCFF0E';
+    setTimeout('color_dos()', 2000);
+}
+
+function color_dos() 
+{
+    textocolor.style.color='#FF0040';
+    setTimeout('color_tres()', 2000);
+}
+
+function color_tres() 
+{
+    textocolor.style.color='#F5A9A9';
+    setTimeout('color_cuatro()', 2000);
+}
+
+function color_cuatro() 
+{
+    textocolor.style.color='#2E9AFE';
+    setTimeout('color_cinco()', 2000);
+}
+
+function color_cinco() 
+{
+    textocolor.style.color='#9AFE2E';
+    setTimeout('color_uno()', 2000);
+}
+
+color_uno()
+
+// Boton Para iniciar/Reiniciar el juego //
+
+$(".btn-reinicio").click(function()
 {
     alert("hola");
+    Cuenta_Atras();
+    
+    $(".btn-reinicio").html('Reiniciar');
 });
+
+// Temporizador //
+
+function Cuenta_Atras() 
+{
+    var Tiempo_Reloj = 5;
+    var Temporizador = document.getElementById('timer');
+    
+    var intervalo = setInterval(function()
+    {
+        Tiempo_Reloj--; 
+        Temporizador.innerHTML = Tiempo_Reloj;
+        if(Tiempo_Reloj == 0)
+        {
+            $(".panel-tablero").hide(1500,'linear');
+            $(".time").hide(1000,'linear');
+
+            $(".score").css("width: 500px");
+            $(".moves").css("width: 500px");
+            
+            $(".btn-reinicio").html('Iniciar');
+        }
+    }, 1000);
+}
 
 //Puntuacion//
 
